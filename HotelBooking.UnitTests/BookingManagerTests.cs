a72af06320e80ee430e0cb0ac729b30367eb0211
Søren <act.wescorn@gmail.com>
2022-09-14 10:53:51 +0200

    Added Test Cases for FindAvailableRoom

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using HotelBooking.Core;
 using HotelBooking.UnitTests.Fakes;
 using Xunit;
@@ -8,13 +10,24 @@ namespace HotelBooking.UnitTests
     public class BookingManagerTests
     {
         private IBookingManager bookingManager;
-
+         
         public BookingManagerTests(){
             DateTime start = DateTime.Today.AddDays(10);
             DateTime end = DateTime.Today.AddDays(20);
             IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
             IRepository<Room> roomRepository = new FakeRoomRepository();
             bookingManager = new BookingManager(bookingRepository, roomRepository);
+        }
+
+        public static IEnumerable<object[]> GetLocalData()
+        {
+            var data = new List<object[]>
+            {
+                new object[] { DateTime.Today.AddDays(1), DateTime.Today.AddDays(10) },
+                new object[] { DateTime.Today.AddDays(10), DateTime.Today.AddDays(15) },
+                new object[] { DateTime.Today.AddDays(11), DateTime.Today.AddDays(25) },
+            };
+            return data;
         }
 
         [Fact]
@@ -40,6 +53,19 @@ namespace HotelBooking.UnitTests
             // Assert
             Assert.NotEqual(-1, roomId);
         }
+
+        [Theory]
+        [MemberData(nameof(GetLocalData))]
+        public void FindAvailableRoom_RoomNotAvailable_RoomIdMinusOne(DateTime startDate, DateTime endDate)
+        {
+            // Arrange
+
+            // Act
+            int roomId = bookingManager.FindAvailableRoom(startDate, endDate);
+            // Assert
+            Assert.Equal(-1, roomId);
+        }
+
 
     }
 }
